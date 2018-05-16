5dd7a09143fbd03e972f983cd53f260df32f7a07
zaymat <may-26@hotmail.fr>
2018-05-16 17:45:42 +0200

    Change table columns

--- db.php
+++ db.php
@@ -21,10 +21,10 @@ function createTables($db){
         $db->exec("DROP TABLE IF EXISTS `authors`");
 
         // Create authors table
-        $db->exec("CREATE TABLE IF NOT EXISTS `authors` (id INT UNSIGNED PRIMARY KEY, `name` VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, `image` VARCHAR(255) NOT NULL, `login` VARCHAR(255) NOT NULL) ENGINE=InnoDB;");
+        $db->exec("CREATE TABLE IF NOT EXISTS `authors` (id INT UNSIGNED PRIMARY KEY, `name` VARCHAR(255), email VARCHAR(255) , `image` VARCHAR(255), `login` VARCHAR(255)) ENGINE=InnoDB;");
 
         // Create commits table
-        $db->exec("CREATE TABLE IF NOT EXISTS `commits` (sha VARCHAR(255) NOT NULL PRIMARY KEY, authorID INT, `date` DATETIME, msg VARCHAR(2048) NOT NULL) ENGINE=InnoDB;");
+        $db->exec("CREATE TABLE IF NOT EXISTS `commits` (sha VARCHAR(255) NOT NULL PRIMARY KEY, authorID INT, authorName VARCHAR(255), committerID INT, committerName VARCHAR(255), `date` DATETIME, msg TEXT, `url` VARCHAR(255)) ENGINE=InnoDB;");
     }
     catch(Exception $e)
     {
@@ -32,32 +32,41 @@ function createTables($db){
     }
 }
 
-
 function commitToDB($commit, $db){
     // parse date to fit SQL requirements
     $date = preg_replace("#^(.+)T(.+)Z$#","$1 $2", $commit["commit"]["author"]["date"]);
 
     // Insert commit in the db
-    $req = $db->prepare("INSERT INTO `commits` VALUES (?, ?, ?, ?)");
+    $req = $db->prepare("INSERT INTO `commits` VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
     $req->execute(array($commit["sha"],
                         $commit["author"]["id"],
+                        $commit["commit"]["author"]["name"],
+                        $commit["committer"]["id"],
+                        $commit["commit"]["committer"]["name"],
                         $date,
-                        $commit["commit"]["message"]));
+                        $commit["commit"]["message"],
+                        $commit["url"]
+                       ));
 
-    // Check if the author is already in the database
-    $res = $db->prepare("SELECT * FROM `authors` WHERE id=?");
-    $res->execute(array($commit["author"]["id"]));
+    // Add both authors and committers to the db
+    $list = ["author", "committer"];
 
-    // Create a new user if the author is not in the db
-    if(!$res->fetch()){
-        $req = $db->prepare("INSERT INTO authors VALUES (?, ?, ?, ?, ?)");
-        $req->execute(array($commit["author"]["id"],
-                            $commit["commit"]["author"]["name"],
-                            $commit["commit"]["author"]["email"],
-                            $commit["author"]["avatar_url"],
-                            $commit["author"]["login"]));
+    foreach($list as $item){
+        // Check if the author is already in the database
+        $res = $db->prepare("SELECT * FROM `authors` WHERE id=?");
+        $res->execute(array($commit[$item]["id"]));
+
+        // Create a new user if the author is not in the db
+        if(!$res->fetch()){
+            $req = $db->prepare("INSERT INTO authors VALUES (?, ?, ?, ?, ?)");
+            $req->execute(array($commit[$item]["id"],
+                                $commit["commit"][$item]["name"],
+                                $commit["commit"][$item]["email"],
+                                $commit[$item]["avatar_url"],
+                                $commit[$item]["login"]));
+        }
+        $res->closeCursor();
     }
-    $res->closeCursor();
 
 }
 
